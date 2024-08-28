7191e3e18b994ea372d163409344aec2a889f930
chris smith <chippsmith@pm.me>
2024-08-28 13:38:21 -0500

    added attestation functionality and added functionality for passes nsec as argument

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # cashu-dlcstr-rs
 A DLC Cashu wallet that use Nostr for coordinations
+
+
+# Resources:
+
+dlc_messages docs: https://docs.rs/dlc-messages/latest/dlc_messages/index.html
+
+kormir: https://github.com/benthecarman/kormir/tree/master
+
+noteduel: https://github.com/MutinyWallet/note-duel/tree/master
+
+nostr sdk docs: https://docs.rs/nostr-sdk/latest/nostr_sdk/
+
+nostr docs: https://docs.rs/nostr/0.34.0/nostr/
+
+dlcspecs:  https://github.com/discreetlogcontracts/dlcspecs/tree/master
+
+dlcspecs resources: https://github.com/discreetlogcontracts/dlcspecs/blob/master/Resources.md
+
+nip88: https://github.com/nostr-protocol/nips/blob/9157321a224bca77b3472a19de72885af9d6a91d/88.md
+
+mutiny blog superposition: https://blog.mutinywallet.com/note-duel-superposition/
+
+nostr  dummy npub: https://primal.net/p/npub1hq4x92twzuam49l6edvf6jcsasl2zssn0zl2nh003jlaetyg95qspf05tt
+
+nostr-sdk source code: https://github.com/rust-nostr/nostr/tree/master/crates/nostr-sdk
+
+nips readme: https://github.com/nostr-protocol/nips/blob/master/README.md
+
+bip340: https://github.com/bitcoin/bips/blob/master/bip-0340.mediawiki
--- src/sub_commands/dlc/dlc.rs
+++ src/sub_commands/dlc/dlc.rs
@@ -4,6 +4,8 @@ use anyhow::Result;
 use clap::{Args, Subcommand};
 use nostr_sdk::{Client, Keys};
 
+use super::{contract_messages::GlobalAttestation, nostr_events::create_attestation_event};
+
 const RELAYS: [&str; 1] = ["wss://relay.damus.io"];
 
 #[derive(Args)]
@@ -15,24 +17,22 @@ pub struct DLCSubCommand {
 #[derive(Subcommand)]
 pub enum DLCCommands {
     Create {
-        arg1: Option<String>, // placeholder for whatever contract metadaa is needed
+        key: String, // placeholder for whatever contract metadaa is needed
         arg2: Option<String>,
     },
-    // Add more subcommands and their arguments as needed
+
+    Attest {
+        key: String,
+    }, // Add more subcommands and their arguments as needed
 }
 
 pub async fn dlc(sub_command_args: &DLCSubCommand) -> Result<()> {
-    let keys =
-        Keys::parse("nsec15jldh0htg2qeeqmqd628js8386fu4xwpnuqddacc64gh0ezdum6qaw574p").unwrap();
-
-    let client = Client::new(&keys);
-    for relay in RELAYS.iter() {
-        client.add_relay(relay.to_string()).await?;
-    }
-    client.connect().await;
+    //let keys =
+    //   Keys::parse("nsec15jldh0htg2qeeqmqd628js8386fu4xwpnuqddacc64gh0ezdum6qaw574p").unwrap();
 
     match &sub_command_args.command {
-        DLCCommands::Create { arg1, arg2 } => {
+        DLCCommands::Create { key, arg2 } => {
+            let keys = Keys::parse(key).unwrap();
             let global_contract_offer = GlobalContractOffer::new();
 
             let event = create_offer_event(
@@ -41,7 +41,37 @@ pub async fn dlc(sub_command_args: &DLCSubCommand) -> Result<()> {
                 &keys,
             )?;
 
+            let client = Client::new(&keys);
+            for relay in RELAYS.iter() {
+                client.add_relay(relay.to_string()).await?;
+            }
+            client.connect().await;
+
             // TODO: create a publish_event function ?
+            match client.send_event(event).await {
+                Ok(event_id) => println!("Event published: {}", event_id.to_string()),
+                Err(e) => eprintln!("Error publishing event: {}", e),
+            }
+        }
+
+        DLCCommands::Attest { key } => {
+            let global_attestation = GlobalAttestation::new();
+
+            let keys = Keys::parse(key).unwrap();
+
+            let event = create_attestation_event(
+                global_attestation,
+                &RELAYS.iter().map(|r| r.to_string()).collect::<Vec<_>>(),
+                &keys,
+            )?;
+
+            let client = Client::new(&keys);
+            for relay in RELAYS.iter() {
+                client.add_relay(relay.to_string()).await?;
+            }
+
+            client.connect().await;
+
             match client.send_event(event).await {
                 Ok(event_id) => println!("Event published: {}", event_id.to_string()),
                 Err(e) => eprintln!("Error publishing event: {}", e),
